11b06cb43fc385dbee90fc2c2ac3f9feaaf11c28
ZephonMakale <mzephon@gmail.com>
2020-11-09 11:03:48 +0300

    Add bootstrap and link

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,7 +9,7 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
   quotes: Quote[] = [
     // tslint:disable-next-line: whitespace
-    new Quote(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son', "Steve",new Date(2020,6,10)),
+    new Quote(1, 'Zephon Makale', 'Steve Jobs', "Your time is limited, so don’t waste it living someone else’s life. Don’t be trapped by dogma – which is living with the results of other people’s thinking.” ",new Date(2020,6,10)),
     new Quote(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son'," Steve ", new Date(2020,6,10)),
     new Quote(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son', "Steve", new Date(2020,6,10)),
 
