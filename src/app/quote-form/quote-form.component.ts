d59ef7365e083c85c6bc837332ef1ed2636e7c85
ZephonMakale <mzephon@gmail.com>
2020-11-09 10:49:22 +0300

    Add the functioning to the quot form ts

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote-form',
   templateUrl: './quote-form.component.html',
-  styleUrls: ['./quote-form.component.css']
+  styleUrls: ['./quote-form.component.css'],
 })
 export class QuoteFormComponent implements OnInit {
+  newQuote = new Quote(0, '', '', '', new Date());
+  @Output() addQuote = new EventEmitter<Quote>();
 
-  constructor() { }
-
-  ngOnInit(): void {
+  // tslint:disable-next-line: typedef
+  submitQuote() {
+    this.addQuote.emit(this.newQuote);
+    this.newQuote = new Quote(0, '', '', '', new Date());
   }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
