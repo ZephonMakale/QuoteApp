c81fdad1942ba3ea0c78b0cf8c73530eb258f624
ZephonMakale <mzephon@gmail.com>
2020-11-09 12:17:49 +0300

    modify header

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -4,14 +4,25 @@ import { Quote } from '../quote';
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
-  styleUrls: ['./quote.component.css']
+  styleUrls: ['./quote.component.css'],
 })
 export class QuoteComponent implements OnInit {
   quotes: Quote[] = [
     // tslint:disable-next-line: whitespace
-    new Quote(1, 'Zephon Makale', 'Steve Jobs', "Your time is limited, so don’t waste it living someone else’s life. Don’t be trapped by dogma – which is living with the results of other people’s thinking.” ",new Date(2020,6,10)),
-    new Quote(2, 'Michael Jordan', 'Kevin Hart', 'Everybody wants to be famous, but nobody wants to do the work. I live by that. You grind hard so you can play hard. At the end of the day, you put all the work in, and eventually it\’ll pay off. It could be in a year, it could be in 30 years. Eventually, your hard work will pay off.', new Date(2020, 6, 10)),
-
+    new Quote(
+      1,
+      'Zephon Makale',
+      'Steve Jobs',
+      'Your time is limited, so don’t waste it living someone else’s life. Don’t be trapped by dogma – which is living with the results of other people’s thinking.” ',
+      new Date(2020, 6, 10)
+    ),
+    new Quote(
+      2,
+      'Michael Jordan',
+      'Kevin Hart',
+      'Everybody wants to be famous, but nobody wants to do the work. I live by that. You grind hard so you can play hard. At the end of the day, you put all the work in, and eventually it’ll pay off. It could be in a year, it could be in 30 years. Eventually, your hard work will pay off.',
+      new Date(2020, 6, 10)
+    ),
   ];
   // tslint:disable-next-line: typedef
   addNewQuote(quote) {
@@ -22,28 +33,28 @@ export class QuoteComponent implements OnInit {
     this.quotes.push(quote);
   }
   // tslint:disable-next-line: typedef
-  deleteQuote(isComplete, index){
+  deleteQuote(isComplete, index) {
     if (isComplete) {
       // tslint:disable-next-line: prefer-const
-      let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].description}?`);
+      let toDelete = confirm(
+        `Are you sure you want to delete ${this.quotes[index].description}?`
+      );
 
-      if (toDelete){
+      if (toDelete) {
         this.quotes.splice(index, 1);
       }
     }
   }
   // tslint:disable-next-line: typedef
-  upvote(index){
+  upvote(index) {
     this.quotes[index].upvote++;
   }
   // tslint:disable-next-line: typedef
-  downvote(index){
+  downvote(index) {
     this.quotes[index].downvote++;
-  // tslint:disable-next-line: no-trailing-whitespace
+    // tslint:disable-next-line: no-trailing-whitespace
   }
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
