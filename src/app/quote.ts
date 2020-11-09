2043b0ce6f814913a583927c1fa2422d08302427
ZephonMakale <mzephon@gmail.com>
2020-11-09 10:44:42 +0300

    Add the forms module to the app module

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -2,7 +2,8 @@ export class Quote {
   upvote: number;
   downvote: number;
   showDescription: boolean;
-  constructor(public id: number, public creator: string,public name: string, public description: string, public date: Date){
+  // tslint:disable-next-line: whitespace
+  constructor(public id: number, public creator: string,public author: string, public description: string, public date: Date){
     this.showDescription = false;
     this.upvote = 0;
     this.downvote = 0;
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,9 +8,10 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
   quotes: Quote[] = [
-    new Quote(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son',"",new Date(2020,6,10)),
-    new Quote(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son',"",new Date(2020,6,10)),
-    new Quote(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son',"",new Date(2020,6,10)),
+    // tslint:disable-next-line: whitespace
+    new Quote(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son', "Steve",new Date(2020,6,10)),
+    new Quote(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son'," Steve ", new Date(2020,6,10)),
+    new Quote(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son', "Steve", new Date(2020,6,10)),
 
   ];
   constructor() { }
